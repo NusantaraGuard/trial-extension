d1eb06451a35398f0d6de407caec7a66c8b3b792
Yazid Al Ghozali <yazid.al2418@gmail.com>
2025-07-05 23:52:24 +0700

    feat: analyze smart contract

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import Home from "./pages/Home"
 import { ThemeProvider } from "./components/ui/theme-provider"
 import AnalyzeAddress from "./pages/AnalyzeAdress"
+import AnalyzeSmartContract from "./pages/AnalyzeSmartContract"
 
 function App() {
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <AnalyzeSmartContract />
       <Home />
       <AnalyzeAddress />
+      {/* Add more pages as needed */}
     </ThemeProvider>
   )
 }
